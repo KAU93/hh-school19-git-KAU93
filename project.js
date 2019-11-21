NasgulNexus/hh-school19-git-crash-course

Stop using alert and use console

project.js
@@ -1,1 +1,1 @@
-alert('Hello world');
+conosle.log('Hello world');

KAU93/hh-school19-git-KAU93

project.js
@@ -1,1 +1,1 @@
-alert('Hello world');
+console.log('Hello world');